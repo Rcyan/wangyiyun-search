05cd0de90d792caf3e235b098d962d38c7dc6f6d
zhengxn <zxn3069@outlook.com>
2017-06-12 20:11:40 +0800

    Create main.js

--- js/main.js
+++ js/main.js
@@ -54,6 +54,7 @@ function getSongs(){
 
                     //3.点击播放
                     var songId=$(this).attr("data-id");
+		    $(".play-iframe").remove();
                     $(".play").click(function(){
                         var oPlay=$("<iframe class='play-iframe' frameborder='no' border='0' marginwidth='0' marginheight='0' width=400 height=86 src='//music.163.com/outchain/player?type=2&id="+songId+"&auto=1&height=66'></iframe>");
                         $(".play-position").append(oPlay);
